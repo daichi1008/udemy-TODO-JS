6064b270ab946ab454c1a45e945d3efec9734789
daichi1008 <forester.dn1008@gmail.com>
2022-01-30 21:42:28 +0900

    戻すボタンの実装

--- index.js
+++ index.js
@@ -2,69 +2,85 @@ const onClickAdd = () =>{
     // テキストボックスの値を取得し、初期化する
     const inputText = document.getElementById("add-text").value;
     document.getElementById("add-text").value="";
+creatIncompleteList(inputText);
+  };
 
+
+  
+
+  //未完了リストに追加する関数
+  const creatIncompleteList = (text) => {
     // div生成
     const div = document.createElement("div");
-    div.className = "list-row"
-    
+    div.className = "list-row";
+
     // liタグ生成
     const li = document.createElement("li");
-    li.innerText = inputText;
-    
-//button（完了）タグの生成
-const completeButton = document.createElement("button");
-completeButton.innerText ="完了";
-completeButton.addEventListener("click", ()=>{
-  
-   //押された完了ボタン(div)を削除
-  deleteFromIncompleteList(completeButton.parentNode);
-//完了リストへ追加する要素
-const addTarget = completeButton.parentNode;
+    li.innerText = text;
 
-//TODO内容テキストを取得
-const text = addTarget.firstElementChild.innerText;
+    //button（完了）タグの生成
+    const completeButton = document.createElement("button");
+    completeButton.innerText = "完了";
+    completeButton.addEventListener("click", () => {
+      //押された完了ボタン(div)を削除
+      deleteFromIncompleteList(completeButton.parentNode);
+      //完了リストへ追加する要素
+      const addTarget = completeButton.parentNode;
 
-//div以下を初期化
-addTarget.textContent =null;
+      //TODO内容テキストを取得
+      const text = addTarget.firstElementChild.innerText;
 
-//liタグを生成
-const li= document.createElement("li")
-li.innerText = text
+      //div以下を初期化
+      addTarget.textContent = null;
 
-//buttonタグ生成
-const backButton = document.createElement("button");
-backButton.innerText="戻す";
+      //liタグを生成
+      const li = document.createElement("li");
+      li.innerText = text;
 
-//divタグの子要素に各要素を設定
-addTarget.appendChild(li);
-addTarget.appendChild(backButton);
+      //buttonタグ生成
+      const backButton = document.createElement("button");
+      backButton.innerText = "戻す";
+      backButton.addEventListener("click", () => {
+        // 押されたボタンの親タグを(div)を完了リストから削除
+        const deleteTarget = backButton.parentNode;
+        document.getElementById("complete-list").removeChild(deleteTarget);
 
-//完了リストに追加
-document.getElementById("complete-List").appendChild(addTarget);
+        //テキストを取得
+        const text = backButton.parentNode.firstElementChild.innerText;
+         creatIncompleteList(text);
+      });
+      //divタグの子要素に各要素を設定
+      addTarget.appendChild(li);
+      addTarget.appendChild(backButton);
 
-})
+      //完了リストに追加
+      document.getElementById("complete-list").appendChild(addTarget);
+    });
 
-//button(削除)タグの生成
-const deleteButton = document.createElement("button");
-deleteButton.innerText = "削除";
-deleteButton.addEventListener("click",()=>{
-  //押された削除ボタン(div)を未完了リストから削除
-  deleteFromIncompleteList(deleteButton.parentNode);
-});
+    //button(削除)タグの生成
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "削除";
+    deleteButton.addEventListener("click", () => {
+      //押された削除ボタン(div)を未完了リストから削除
+      deleteFromIncompleteList(deleteButton.parentNode);
+    });
 
-//divタグの子要素に各要素を設定
-div.appendChild(li);
-div.appendChild(completeButton);
-div.appendChild(deleteButton);
+    //divタグの子要素に各要素を設定
+    div.appendChild(li);
+    div.appendChild(completeButton);
+    div.appendChild(deleteButton);
 
-//未完了のリストへ追加
-document.getElementById("incomplete-list").appendChild(div);
- 
-};
-document.getElementById("add-button")
-  .addEventListener("click", () => onClickAdd());
+    //未完了のリストへ追加
+    document.getElementById("incomplete-list").appendChild(div);
+  };
+  document
+    .getElementById("add-button")
+    .addEventListener("click", () => onClickAdd());
 
   //未完了リストから指定の要素を削除
-  const deleteFromIncompleteList= (target)=>{
-     document.getElementById("incomplete-list").removeChild(target);
-  }
+  const deleteFromIncompleteList = (target) => {
+    document.getElementById("incomplete-list").removeChild(target);
+  };
+    
+
+  
